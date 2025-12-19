a6fa5f2ac8bcd2ce8a7ffff46c9581b839773abb
devos10 <osbaldo081004@gmail.com>
2025-12-18 22:59:42 -0600

    documentado codigo

--- app/Controllers/LoginControlador.php
+++ app/Controllers/LoginControlador.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Controllers;
-
+//clases usadas
 use App\Core\Controlador;
 use App\Core\Validador;
 use App\Core\AlertaFlash;
@@ -24,44 +24,51 @@ class LoginControlador extends Controlador
 
     public function verificar(): void
     {
+        //verificamos que se envie por POST si no redigire al login
         if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
             header('Location: ?controlador=login&accion=index');
             exit;
         }
-
+        //recibimos las variables post 
         $usuario   = $_POST['usuario'] ?? '';
         $password  = $_POST['password'] ?? '';
-
+        //hacemos uso del validador para veirificar que las variables no esten vacias y limpiarlas
         $validador = new Validador();
         $validador->validarNoVacio('usuario', $usuario);
         $validador->validarNoVacio('password', $password);
         $usuario = $validador->limpiarCadena($usuario);
         $password = $validador->limpiarCadena($password);
-
+        //si el validador nos devuelve errores 
         if ($validador->tieneErrores()) {
+            //generamos variable de sesion con los datos de los camps que se enviaron y recibieron
             $_SESSION['old'] = ['usuario' => $usuario];
 
             $items = '';
+            //recorremos los errores
             foreach ($validador->obtenerErrores() as $campo => $mensajes) {
                 foreach ($mensajes as $msg) {
+                    //guardamos en items cada error 
                     $items .= '<li>' . htmlspecialchars($msg, ENT_QUOTES, 'UTF-8') . '</li>';
                 }
             }
-
+            //hacemos uso de la clase alerta para generar una alerta con los multiples errores
             AlertaFlash::error('Corrige el formulario', '', [
                 'html' => '<ul style="text-align:left; margin:0; padding-left:18px;">' . $items . '</ul>',
                 'icon' => 'error',
             ]);
-
+            //la redireccion es al mismo login
             header('Location: ?controlador=login&accion=index');
             exit;
         }
-
+        //creamos una variable del modelo
         $modelo = new LoginModelo($this->pdo);
+        //creamos una variable que guardara los datos retornados por el metodo del modelo
         $datosLogin = $modelo->validarUsuario($usuario, $password);
-
+        //verificamos si datos contiene informacion 
         if ($datosLogin) {
-            Autenticacion::login($datosLogin);
+            //si tenemos datos hacemos uso de la clase autenticacion, usando el metodo login y enviendole los datos
+            Autenticacion::login($datosLogin); //crea un array de sesion con los datos del usuario legueado
+            //se muestra alerta exitosa
             AlertaFlash::exito(
                 "Inicio correcto",
                 "Inicio de sesión exitoso",
@@ -73,9 +80,13 @@ class LoginControlador extends Controlador
                     'timerProgressBar' => true
                 ]
             );
+            //redireccionamos al dashboard del entrenador
             header('Location: ?controlador=entrenador&accion=index');
             exit;
+            //si no se recibieron datos
         } else {
+            //se envia una alerta tipo modal indicando que las credenciales no son validas
+
             AlertaFlash::error(
                 "Usuario Inválido",
                 "Usuario o Contraseña incorrectos!!!",
@@ -89,6 +100,7 @@ class LoginControlador extends Controlador
                     'icon' => 'error',
                 ]
             );
+            //redireccionamos de nuevo al login 
             header('Location: ?controlador=login&accion=index');
             exit;
         }
