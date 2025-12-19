e8971f76bb02a67e4391e740cc4fe80c31aa113f
devos10 <osbaldo081004@gmail.com>
2025-12-18 18:23:27 -0600

    head dinamico para archivos css

---
+++ app/Views/partials/head.php
@@ -0,0 +1,25 @@
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= $titulo; ?></title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css" rel="stylesheet">
+    <!-- Instascan para leer QR -->
+    <script src="https://rawgit.com/schmich/instascan-builds/master/instascan.min.js"></script>
+    <!-- Bootstrap Bundle con Popper.js -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <!--Para buscar mas rapido a los clientes cuando se haga el primer pago
+     CSS de Select2 -->
+    <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+    <!-- jQuery -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <!-- JS de Select2 -->
+    <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+
+    <?php if (!empty($css) && is_array($css)): ?>
+    <?php foreach ($css as $href): ?>
+      <link rel="stylesheet" href="/sistema_gimnasio_mvc/app<?= htmlspecialchars($href, ENT_QUOTES, 'UTF-8') ?>">
+    <?php endforeach; ?>
+  <?php endif; ?>
+</head>
