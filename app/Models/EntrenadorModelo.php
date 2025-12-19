def74eef0de1da7e0e22643c25d1993f5fe9855c
devos10 <osbaldo081004@gmail.com>
2025-12-19 17:05:37 -0600

    Modelo del entrenador vacio

--- app/Models/EntrenadorModelo.php
+++ app/Models/EntrenadorModelo.php
@@ -5,22 +5,7 @@ namespace App\Models;
 use App\Core\MainModel;
 class EntrenadorModelo extends MainModel
 {
-    public function crearSocio(array $datosSocio): int
-    {
-        $this->ejecutarConsulta("
-            INSERT INTO cliente (
-                nombre, apellido1, apellido2, edad,
-                numero_telefono, contacto_familiar,
-                descripcion_medica, foto, id_categoria, qr
-            ) VALUES (
-                :nombre, :apellido1, :apellido2, :edad,
-                :numero_telefono, :contacto_familiar,
-                :descripcion_medica, :foto, :id_categoria, :qr
-            )
-        ", $datosSocio);
-
-        return (int) $this->pdo->lastInsertId();
-    }
+   
 
 
 }
