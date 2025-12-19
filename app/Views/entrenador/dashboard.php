7d12fd6cc7d4737a7a7baffb06a4c979397a29e9
devos10 <osbaldo081004@gmail.com>
2025-12-18 21:46:31 -0600

    incluyendo archivo del head

--- app/Views/entrenador/dashboard.php
+++ app/Views/entrenador/dashboard.php
@@ -1,10 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?= $titulo ?></title>
-</head>
+<?php include_once (APP_URL.'Views/partials/head.php');?>
 <body>
    <h1><?= $mensaje ?></h1> 
 </body>
