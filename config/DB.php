41cf61af90585a38888cd559b508a7e1e24df983
devos10 <111657011+devos10@users.noreply.github.com>
2025-12-18 19:07:37 -0600

    Constantes para la base de datos limpio

--- config/DB.php
+++ config/DB.php
@@ -2,7 +2,7 @@
 
 /*Constantes de la base de datos */
     const DB_GESTOR='mysql';
-    const DB_SERVER='localhost';
-    const DB_NAME='empleados';
-    const DB_USER='root';
-    const DB_PASS='100408ovc';
+    const DB_SERVER='';
+    const DB_NAME='';
+    const DB_USER='';
+    const DB_PASS='';
