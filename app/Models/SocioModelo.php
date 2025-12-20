c25f91853c6f1cd6c8d5c5aab4fc8f7898a86a93
devos10 <osbaldo081004@gmail.com>
2025-12-19 19:49:42 -0600

    Agregamos consulta para traer la informacion de todos los socios, limitado solo a 10 registros por el momento

--- app/Models/SocioModelo.php
+++ app/Models/SocioModelo.php
@@ -1,13 +1,13 @@
 <?php
 namespace App\Models;
 use App\Core\MainModel;
-
+use PDO;
 class SocioModelo extends MainModel{
     
      public function crearSocio(array $datosSocio): int
     {
         $this->ejecutarConsulta("
-            INSERT INTO cliente (
+            INSERT INTO CLIENTE (
                 nombre, apellido1, apellido2, edad,
                 numero_telefono, contacto_familiar,
                 descripcion_medica, foto, id_categoria, qr
@@ -19,5 +19,29 @@ class SocioModelo extends MainModel{
         ", $datosSocio);
 
         return (int) $this->pdo->lastInsertId();
+    }
+
+    public function listarSocios(){
+        $sql=$this->ejecutarConsulta(
+        "SELECT 
+            c.id_cliente,
+            c.nombre,
+            c.apellido1,
+            c.apellido2,
+            c.edad,
+            c.numero_telefono,
+            c.contacto_familiar,
+            c.descripcion_medica,
+            c.foto,
+            c.qr,
+            c.id_categoria,
+            cat.nombre AS nombre_categoria
+        FROM CLIENTE c
+		LEFT JOIN 
+            CATEGORIA cat ON c.id_categoria= cat.id_categoria
+        ORDER BY c.id_cliente ASC LIMIT 10;"
+        );
+        $clientes = $sql->fetchAll(PDO::FETCH_ASSOC);
+        return $clientes; // lista de todos los clientes con informacion
     }
 }
